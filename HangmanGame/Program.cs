67b933eebdcbcdd884a9db4e6f8bb5a6dd3eeb42
Rayan Hadjari <rayan.ha@me.com>
2022-08-24 14:47:56 +0200

    first commit

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("WELCOME TO HANGMAN");
+Console.WriteLine();
+Console.WriteLine("Press ENTER to start");
+
+Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("GUESS A LETTER!");
+Console.ReadLine();
+
+Console.ReadLine();
